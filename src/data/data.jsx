a78d557fef864a5254d2c306ad1b08f348c44c49
Endor098 <selaevtimur1@gmail.com>
2025-11-08 16:30:15 +0500

    add Header

---
+++ src/data/data.jsx
@@ -0,0 +1,89 @@
+import HeaderBasketIcon from "../assets/icon/HeaderBasketIcon";
+import HeaderComparatorIcon from "../assets/icon/HeaderComparatorIcon";
+import HeaderFooterHomeIcon from "../assets/icon/HeaderFooterHomeIcon";
+import HeaderHEartIcon from "../assets/icon/HeaderHEartIcon";
+import HeaderPersonIcon from "../assets/icon/HeaderPersonIcon";
+
+export const headerfooter =[
+    {
+        id: 1,
+        path:"/",
+        icon: <HeaderFooterHomeIcon/>,
+        title: "Bosh sahifa"
+    },
+    {
+        id: 2,
+        path:"/product-item",
+        icon: <HeaderComparatorIcon />,
+        title: "Bosh sahifa"
+    },
+    {
+        id: 3,
+        path:"/favourite",
+        icon: <HeaderHEartIcon />,
+        title: "Bosh sahifa"
+    },
+    {
+        id: 4,
+        path:"/cart",
+        icon: <HeaderBasketIcon />,
+        title: "Bosh sahifa"
+    },
+    {
+        id: 5,
+        path:"/",
+        icon: <HeaderPersonIcon />,
+        title: "Bosh sahifa"
+    },
+]
+
+export const headermenuleft = [
+    {
+        id: 1,
+        title: "Smartfonlar va telefonlar"
+    },
+    {
+        id: 2,
+        title: "TV, audio va video"
+    },
+    {
+        id: 3,
+        title: "Maishiy texnika"
+    },
+    {
+        id: 4,
+        title: "Go'zallik va so'glik"
+    },
+    {
+        id: 5,
+        title: "Noutbuklar va kompyuterlar"
+    },
+    {
+        id: 6,
+        title: "Gadjetlar va aksessuarlar"
+    },
+    {
+        id: 7,
+        title: "Avto zona"
+    },
+    {
+        id: 8,
+        title: "Ta'mirlash uchun asboblar"
+    },
+    {
+        id: 9,
+        title: "Bolalar uchun tovarlar"
+    },
+    {
+        id: 10,
+        title: "Video kuzatuv tizimlari"
+    },
+    {
+        id: 11,
+        title: "Ofis va uy uchun mebel"
+    },
+    {
+        id: 12,
+        title: "Oshxona jixozlari"
+    },
+]
